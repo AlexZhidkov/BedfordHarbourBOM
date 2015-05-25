ac784f8efb536d0249eb4b29897f4937376b3a39
AlexZhidkov <azhidkov@gmail.com>
2015-05-25 12:15:57 +0800

    Reworked Supplier

--- Bom.Client.Entities/Supplier.cs
+++ Bom.Client.Entities/Supplier.cs
@@ -3,13 +3,60 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FluentValidation;
 
 namespace Bom.Client.Entities
 {
     public class Supplier : BaseEntity
     {
-        public string Name { get; set; }
-        public string Contact { get; set; }
-        public string Phone { get; set; }
+        private string _name;
+        private string _contact;
+        private string _phone;
+
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                if (_name == value) return;
+                _name = value;
+                OnPropertyChanged(() => Name);
+            }
+        }
+
+        public string Contact
+        {
+            get { return _contact; }
+            set
+            {
+                if (_contact == value) return;
+                _contact = value;
+                OnPropertyChanged(() => Contact);
+            }
+        }
+
+        public string Phone
+        {
+            get { return _phone; }
+            set
+            {
+                if (_phone == value) return;
+                _phone = value;
+                OnPropertyChanged(() => Phone);
+            }
+        }
+
+        class SupplierValidator : AbstractValidator<Supplier>
+        {
+            public SupplierValidator()
+            {
+                RuleFor(obj => obj.Name).NotEmpty();
+            }
+        }
+
+        protected override IValidator GetValidator()
+        {
+            return new SupplierValidator();
+        }
     }
 }
