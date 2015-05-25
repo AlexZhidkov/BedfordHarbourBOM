6277dd9ed50e53948c10424e0518b064c3736da2
AlexZhidkov <azhidkov@gmail.com>
2015-05-25 12:02:22 +0800

    Added new files

--- Bom.Client.Entities/BaseEntity.cs
+++ Bom.Client.Entities/BaseEntity.cs
@@ -3,12 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Core.Common.Core;
 
 namespace Bom.Client.Entities
 {
-    public abstract class BaseEntity
+    public abstract class BaseEntity : ObjectBase
     {
-        public int Id { get; set; }
-        public string Notes { get; set; }
+        private int _id;
+        private string _notes;
+
+        public int Id
+        {
+            get { return _id; }
+            set
+            {
+                if (_id != value)
+                {
+                    _id = value;
+                    OnPropertyChanged(() => Id);
+                }
+            }
+        }
+
+        public string Notes
+        {
+            get { return _notes; }
+            set
+            {
+                if (_notes != value)
+                {
+                    _notes = value;
+                    OnPropertyChanged(() => Notes);
+                }
+            }
+        }
+
     }
 }
