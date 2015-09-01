1574ada9e31376cfe40b0486a9b1c4ad3f72c1a5
Alexander Zhidkov <azhidkov@gmail.com>
2015-09-01 10:52:32 +0800

    ProductTreeView WIP

---
+++ Bom.Desktop.Tests/ProductTreeViewModelTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Bom.Desktop.Tests
+{
+    [TestClass]
+    public class ProductTreeViewModelTest
+    {
+        [TestMethod]
+        public void StopBuild()
+        {
+            Assert.Fail("Remove this test when code ready for release");
+        }
+    }
+}
